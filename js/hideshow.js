a1ce1e63cd20d3a0e56fee013f6a6a05c5c13287
Carmen Vargas <genplas@ymail.com>
2018-07-24 11:28:31 -0400

    Update

--- js/hideshow.js
+++ js/hideshow.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 	$("#search").on("keypress", function (){
 		var search = $(this).text().toLowerCase();
-		$("img").each(function(){
-			if ($(this).text().toLowerCase().indexOf(search) > 1) {
+		$("img").filter(function(){
+			if ($(this).text().toLowerCase().indexOf(search) > -1) {
 				$(this).show();
 			} else {
 				$(this).hide();
 			}
-		})
+		});
 	});
 });
