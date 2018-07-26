38d9d41e159ab35b5433b67542bc00f06a6650ff
Carmen Vargas <genplas@ymail.com>
2018-07-26 11:39:01 -0400

    Update

--- js/hideshow.js
+++ js/hideshow.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
 	$("#search").on("keypress", function (){
-		var search = $(this).text().toLowerCase();
+		var search = $(this).val().toLowerCase();
 		$("img").filter(function(){
-			if ($(this).text().toLowerCase().indexOf(search) > -1) {
+			if ($(this).attr("alt").toLowerCase().indexOf(search) > -1) {
 				$(this).show();
 			} else {
 				$(this).hide();
